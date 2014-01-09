09ce0f2fc93d7466dc63b2905a7bafba999bd281
Malte Lenz <malte.lenz@gmail.com>
2014-01-09 21:34:25 +0100

    Start on layout

--- Android/src/com/maltelenz/climbscore/MainActivity.java
+++ Android/src/com/maltelenz/climbscore/MainActivity.java
@@ -1,8 +1,10 @@
 package com.maltelenz.climbscore;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -19,5 +21,10 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+ 
     
+    public void addClimb(View view) {
+    	Intent intent = new Intent(this, AddClimbActivity.class);
+    	startActivity(intent);
+    }
 }
